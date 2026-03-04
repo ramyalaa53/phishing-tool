65e4103f3b45eb29d633a623f0487e84e958eade
ramyalaa53 <ralaaamro@gmail.com>
2026-03-05 01:39:26 +0200

    fix render port

--- app.py
+++ app.py
@@ -343,9 +343,9 @@ def create_app():
     return app
 
 
-if __name__ == '__main__':
+import os
+
+if __name__ == "__main__":
     app = create_app()
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    print(f"Server running on http://{local_ip}:555")
-    app.run(host='0.0.0.0', port=555, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
