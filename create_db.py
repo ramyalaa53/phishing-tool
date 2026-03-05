b2465abb83e351c5a60888a5da01dfc979c4ee26
ramyalaa53 <ralaaamro@gmail.com>
2026-03-05 03:13:25 +0200

    fix password hashing

--- create_db.py
+++ create_db.py
@@ -7,9 +7,9 @@ app.app_context().push()
 
 db.create_all()
 
-if not User.query.filter_by(email='admin@example.com').first():
-    admin = User(name='Admin', email='admin@example.com', role='admin',
-                 password_hash=generate_password_hash('1234'))
+if not User.query.filter_by(email='ramyalaa100@gmail.com').first():
+    admin = User(name='Admin', email='ramyalaa100@gmail.com', role='admin',
+                 password_hash=generate_password_hash('246810'))
     db.session.add(admin)
     db.session.commit()
     print("Created default admin: admin@example.com / 1234")
