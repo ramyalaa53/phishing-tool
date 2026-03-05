d78ae60c94619199ab82e34d6a8fb128210e5550
ramyalaa53 <ralaaamro@gmail.com>
2026-03-05 03:19:42 +0200

    create database tables

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
     login_manager = LoginManager()
     login_manager.login_view = 'login'
     login_manager.init_app(app)
